b703e843fa7717409aff9c15565f7ae67e053a38
Nyldesu <aldrinnyl123@gmail.com>
2025-12-22 10:36:05 +0800

    Pasko Text

--- frontend/src/pages/Help.jsx
+++ frontend/src/pages/Help.jsx
@@ -1,7 +1,7 @@
 export default function Help({}){
   return(
     <div className="text-center justify-center content-center align-middle h-full">
-      <h1 className="text-3xl font-semibold text-sky-400">HELP ME, GUSTO NAKO MAKA FEEL SA SUMMER</h1>
+      <h1 className="text-4xl font-semibold text-sky-400">HELP ME, GUSTO NAKO MAKA FEEL SA PASKO</h1>
     </div>
   );
 
